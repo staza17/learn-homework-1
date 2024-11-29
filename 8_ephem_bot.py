110bcf782b4f5dcae477066b86121f1bb7c1b7c2
Anastasia Bespalenko <bluestaza@yandex.ru>
2024-11-29 16:27:35 +0300

    Готовая домашняя работа

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -15,43 +15,76 @@
 import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import settings
+import ephem
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log')
 
 
-PROXY = {
-    'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {
-        'username': 'learn',
-        'password': 'python'
-    }
-}
-
+zodiacs = {'Aries': 'Овен', 'Leo': 'Лев', 'Sagittarius': 'Стрелец',
+           'Taurus': 'Телец', 'Virgo': 'Дева', 'Capricorn': 'Козерог',
+            'Gemini': 'Близнецы', 'Libra,': 'Весы', 'Aquarius': 'Водолей',
+           'Cancer': 'Рак', 'Scorpio': 'Скорпион', 'Pisces': 'Рыбы', 'Ophiuchus': 'Змееносец'}
 
 def greet_user(update, context):
-    text = 'Вызван /start'
-    print(text)
-    update.message.reply_text(text)
+    print('Вызван /start')
+    update.message.reply_text('Привет!')
 
-
-def talk_to_me(update, context):
-    user_text = update.message.text
-    print(user_text)
-    update.message.reply_text(text)
+def planet_check(update, context):
+    print('Вызван /planet')
+    if len(update.message.text.lower().split()) < 2:
+        update.message.reply_text('Некорректный запрос')
+    user_text = update.message.text.lower().split()[1]
+    if user_text == 'mars':
+        planet = ephem.Mars('2024/11/29')
+        constellation = ephem.constellation(planet)
+        print(constellation)
+        update.message.reply_text(zodiacs[constellation[1]])
+    elif user_text == 'mercury':
+        planet = ephem.Mercury('2024/11/29')
+        constellation = ephem.constellation(planet)
+        print(constellation)
+        update.message.reply_text(zodiacs[constellation[1]])
+    elif user_text == 'venus':
+        planet = ephem.Venus('2024/11/29')
+        constellation = ephem.constellation(planet)
+        print(constellation)
+        update.message.reply_text(zodiacs[constellation[1]])
+    elif user_text == 'jupiter':
+        planet = ephem.Jupiter('2024/11/29')
+        constellation = ephem.constellation(planet)
+        print(constellation)
+        update.message.reply_text(zodiacs[constellation[1]])
+    elif user_text == 'saturn':
+        planet = ephem.Saturn('2024/11/29')
+        constellation = ephem.constellation(planet)
+        print(constellation)
+        update.message.reply_text(zodiacs[constellation[1]])
+    elif user_text == 'uranus':
+        planet = ephem.Uranus('2024/11/29')
+        constellation = ephem.constellation(planet)
+        print(constellation)
+        update.message.reply_text(zodiacs[constellation[1]])
+    elif user_text == 'neptune':
+        planet = ephem.Neptune('2024/11/29')
+        constellation = ephem.constellation(planet)
+        print(constellation)
+        update.message.reply_text(zodiacs[constellation[1]])
+    else:
+        update.message.reply_text('Некорректный запрос')
 
 
 def main():
-    mybot = Updater("КЛЮЧ, КОТОРЫЙ НАМ ВЫДАЛ BotFather", request_kwargs=PROXY, use_context=True)
+    mybot = Updater(settings.API_KEY, use_context=True)
 
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler('start', greet_user))
+    dp.add_handler(CommandHandler('planet', planet_check))
 
     mybot.start_polling()
     mybot.idle()
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
